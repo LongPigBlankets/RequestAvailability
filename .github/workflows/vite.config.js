a7c672017c0f3aed853b1a2f6d21336bc2f2bb3b
LongPigBlankets <urigiuccristian@gmail.com>
2025-08-18 16:47:47 +0100

    Update vite.config.js

--- .github/workflows/vite.config.js
+++ .github/workflows/vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: '/<RequestAvailability>/', // required for GitHub Pages
+  base: '/RequestAvailability/', // required for GitHub Pages
 })
